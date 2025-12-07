8c4f53d9218a3f5cf97f49e6562d317457845fd1
Jhon Ospina <jhonospina2711@gmail.com>
2025-12-06 23:11:32 -0500

    Se define la interface IProductRepository

---
+++ WebApiEcommerce/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using WebApiEcommerce.Model;
+
+namespace WebApiEcommerce.Repository.IRepository;
+
+public interface IProductRepository 
+{
+    ICollection<Product> GetProducts();
+    ICollection<Product> GetProductsForCategory(int categoryId);
+    ICollection<Product> SearchProduct(string name);
+    Product? GetProduct(int productId);
+    bool BuyProduct(string Name, int quantity);
+    bool ProductExists(int Id);
+    bool ProductExists(string Name);
+    bool UpdateProduct(Product product);
+    bool DeleteProduct(Product product);
+    bool Save();
+
+
+}
